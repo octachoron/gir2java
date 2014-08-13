757f543f7fbea5c069d8a032d32501c73bb6fc78
Roland Elek <elek.roland@gmail.com>
2014-08-13 06:08:42 +0200

    Add missing isInstance flag to ParameterDescriptor

--- src/gir2java/ParameterDescriptor.java
+++ src/gir2java/ParameterDescriptor.java
@@ -5,10 +5,12 @@ import java.util.List;
 public class ParameterDescriptor {
 	private String name;
 	private ConvertedType type;
+	private boolean isInstance;
 	
 	public ParameterDescriptor(String name, ConvertedType type, boolean isInstance) {
 		this.name = name;
 		this.type = type;
+		this.isInstance = isInstance;
 	}
 
 	public String getName() {
@@ -23,6 +25,10 @@ public class ParameterDescriptor {
 		return (type == null);
 	}
 	
+	public boolean isInstance() {
+		return isInstance;
+	}
+
 	public static boolean containsPointer(List<ParameterDescriptor> list) {
 		for (ParameterDescriptor desc : list) {
 			if (desc.getType().isPointer()) {

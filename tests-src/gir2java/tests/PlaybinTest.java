dd0209bbc577354feb4cd7ce05ac4ad4c3aae470
Roland Elek <elek.roland@gmail.com>
2014-08-18 04:44:03 +0200

    Add a playbin test, a quick test runner, and a small test video
    ant run-tests should now also play test.ogv for 5 seconds.

---
+++ tests-src/gir2java/tests/PlaybinTest.java
@@ -0,0 +1,72 @@
+package gir2java.tests;
+
+import generated.Gst;
+import generated.glib20.glib.GMainLoop;
+import generated.gobject20.gobject.GObject;
+import generated.gstreamer10.gst.GstElement;
+import generated.gstreamer10.gst.GstElementFactory;
+import generated.gstreamer10.gst.GstPipeline;
+import generated.gstreamer10.gst.GstState;
+
+import java.io.IOException;
+
+import org.bridj.Pointer;
+
+public class PlaybinTest {
+
+	public static class Shutdown implements Runnable {
+		private GMainLoop mainLoop;
+		private long timeoutMillis;
+		
+		public Shutdown(GMainLoop mainLoop, long timeoutMillis) {
+			this.mainLoop = mainLoop;
+			this.timeoutMillis = timeoutMillis;
+		}
+		
+		@Override
+		public void run() {
+			try {
+				Thread.sleep(timeoutMillis);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			mainLoop.quit();
+		}
+	}
+	
+	public static void main(String[] args) {
+		Gst.init(null, null);
+		
+		Pointer<GMainLoop> mainLoopPointer = GMainLoop._new(null, false).as(GMainLoop.class);
+		Pointer<GstElement> playbin = (Pointer<GstElement>)GstElementFactory.make(
+				Pointer.pointerToCString("playbin"),
+				Pointer.pointerToCString("the_playbin")
+		);
+		
+		if (playbin == null) {
+			System.err.println("The playbin could not be created.");
+			return;
+		}
+		
+		String uri = "file://" + System.getProperty("user.dir") + "/test.ogv";
+		System.out.println("Trying to play " + uri);
+		GObject.set(
+				playbin,
+				Pointer.pointerToCString("uri"),
+				Pointer.pointerToCString(uri),
+				null);
+		
+		GstPipeline pipeline = playbin.as(GstPipeline.class).get();
+		pipeline.set_state(GstState.GST_STATE_PLAYING);
+		GMainLoop mainLoop = mainLoopPointer.get();
+		System.out.println("Everything seems OK so far, starting main loop");
+		//TODO: use a proper bus watch to wait for EOS
+		new Thread(new Shutdown(mainLoop, 5000), "Shutdown thread").start();
+		mainLoop.run();
+		//The fun stuff happens, then:
+		System.out.println("Main loop finished");
+		pipeline.set_state(GstState.GST_STATE_NULL);
+		//FIXME: unref is not generated for some reason
+	}
+}
---
+++ tests-src/gir2java/tests/TestRunner.java
@@ -0,0 +1,12 @@
+package gir2java.tests;
+
+import java.io.IOException;
+
+public class TestRunner {
+	
+	public static void main(String[] args) throws IOException {
+		GListTest.main(null);
+		PlaybinTest.main(null);
+	}
+	
+}
